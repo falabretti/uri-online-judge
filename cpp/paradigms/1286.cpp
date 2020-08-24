3f1721ebd58f913b00971dad9ae2c1763a7caea3
Lucas Falabretti <lucasfalabretti7@gmail.com>
2020-08-24 19:35:49 -0300

    Add 1286.cpp

---
+++ cpp/paradigms/1286.cpp
@@ -0,0 +1,53 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int knapsack(int weights[], int values[], int qnt, int max_weight) {
+
+    int dp[qnt + 1][max_weight + 1];
+
+    for (int i = 0; i <= qnt; i++) {
+        for (int j = 0; j <= max_weight; j++) {
+
+            if (i == 0 or j == 0) {
+                dp[i][j] = 0;
+                continue;
+            }
+
+            int without = dp[i - 1][j];
+            int with = 0;
+
+            if (weights[i] <= j) {
+                with = values[i] + dp[i - 1][j - weights[i]];
+            }
+
+            dp[i][j] = max(without, with);
+        }
+    }
+
+    return dp[qnt][max_weight];
+}
+
+int main() {
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int n;
+    while (cin >> n and n) {
+        int p;
+        cin >> p;
+
+        int weights[n + 1];
+        int values[n + 1];
+
+        for (int i = 1; i <= n; i++) {
+            cin >> values[i] >> weights[i];
+        }
+
+        int best = knapsack(weights, values, n, p);
+        cout << best << " min.\n";
+    }
+
+    return 0;
+}
